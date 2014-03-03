11b66e41abb68c7245157f7fadd7e51065b67722
dray <dray@westmont.edu>
2014-03-02 23:33:04 -0800

    update to work with new admissions data

--- prepare_data.rb
+++ prepare_data.rb
@@ -8,20 +8,28 @@ class PrepareData
   Test_Data_Folder = "test_data"
 
   # indices of attributes from the starting data file that should not appear in the prepared data
-  Reject_Indices = [18,19,20,21]
+  Reject_Indices = [0,19,20,21,33]
+  Admit_Date_Index = 19
+  Enrollment_Date_Index = 20
 
   # ID3 specific values
-  Attributes = [:gender, :ethnicity, :state, :postal_code, :country, :entry_term, :enter_level, :application_status, :major1, :major2, :major3, :hs_CEEB_code, :hSGPA, :coll_cum_GPA, :grad_year, :ACT_score, :SAT_score, :application_date, :appl_rating, :appl_calc_rating, :app_misc2, :initial_visit_code, :initial_visit_date, :complete_app]
-  Attribute_Types = [:discrete, :discrete, :discrete, :discrete, :discrete, :discrete, :discrete, :discrete, :discrete, :discrete, :discrete, :discrete, :continuous, :continuous, :continuous, :continuous, :continuous, :continuous, :continuous, :continuous, :discrete, :discrete, :continuous, :discrete]
+  Attributes = [:gender, :ethnicity, :state, :postal_code, :country, :major1, :major2, :major3, :hs_CEEB_code, :hSGPA, :grad_year, :ACT_score, :SAT_score, :application_date, :initial_visit_code, :initial_visit_date, :initial_inquiry_code, :initial_inquiry_date, :first_source_app, :entry_term, :opportunity_type, :application_status, :scholarships, :scholarship_bumped, :academic_index, :academic_year, :total_family_income, :expected_family_contribution, :total_gift_aid]
+  Attribute_Types = ["discrete", "discrete", "discrete", "discrete", "discrete", "discrete", "discrete", "discrete", "discrete", "continuous", "discrete", "continuous", "continuous", "discrete", "discrete", "discrete", "discrete", "discrete", "discrete", "discrete", "discrete", "discrete", "discrete", "discrete", "continuous", "discrete", "continuous", "continuous", "continuous"]
   Default_Value = "no_admit"
 
   # ANN specific values
-  Discrete_Options = [2,5,50,1,0,2,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,1,2] # the number of nodes to use in representing each discrete attribute (1 => continuous attribute)
-  Continuous_Denominators = [0,0,0,100000,0,0,0,0,0,0,0,0,5,4,2100,36,1600,-1,100,100,0,0,-1,0] # the number to divide each continuous attribute by to produce values <= 1 (0 => discrete attribute)
+  # unknown discrete options: High School CEEB Code, Grad Year, Application date,Initial Visit Date,Date of First Inquiry,Entry Term,Acad Year
+  Discrete_Options = [2,10,60,1,196,113,113,113,0,1,0,1,1,0,6,0,131,0,2,0,4,37,9,4,1,0,1,1,1] # the number of nodes to use in representing each discrete attribute (1 => continuous attribute)
+  Continuous_Denominators = [0,0,0,100000,0,0,0,0,0,5,0,36,1600,0,0,0,0,0,0,0,0,0,0,0,10000,0,100000000,100000000,100000] # the number to divide each continuous attribute by to produce values <= 1 (0 => discrete attribute)
   ANN_Class_Values = {"no_admit" => "1 0 0", "admit_no_matriculate" => "0 1 0", "admit_matriculate" => "0 0 1"}
 
   # initialized with a csv file containing data instances for training/testing
-  def initialize(data_file)
+  def initialize(training_data = {}, test_data = {}, data_file = "admissions_data.csv")
+    Classifiers.each do |classifier|
+      training_files[classifier] ||= "#{Training_Data_Folder}/#{classifier.to_s}_training_data.txt"
+      test_files[classifier] ||= "#{Test_Data_Folder}/#{classifier.to_s}_test_data.txt"
+    end
+
     @training_data = []
     @test_data = []
     File.open(data_file) do |f|
@@ -29,18 +37,19 @@ class PrepareData
       f.each_line do |line|
         line.gsub!(/".*,.*"/) {|attribute| "\\#{attribute.gsub(',','')}"} #remove commas contained in matched quotes
         instance_array = line.chomp.split(',')
+        class_value = determine_class_value(instance_array)
         (Attributes.size + Reject_Indices.size - instance_array.size).times {instance_array << ""} #ensure the appropriate number of attributes in case trailing empty strings are dropped
-        class_info = []
-        Reject_Indices.each {|n| class_info << instance_array.delete_at(n)}
+        Reject_Indices.sort! {|m,n| n <=> m} # sort indices in descending order
+        Reject_Indices.each {|n| instance_array.delete_at(n)}
         if rand(3) == 0
-          @test_data << [instance_array, determine_class_value(class_info)]
+          @test_data << [instance_array, class_value]
         else
-          @training_data << [instance_array, determine_class_value(class_info)]
+          @training_data << [instance_array, class_value]
         end
       end
     end
     @discrete_options_to_numbers = []
-    @training_data.first.first.size.times {@discrete_options_to_numbers << {}}
+    @training_data.first.first.size.times {@discrete_options_to_numbers << {}} # contains one hash per attribute
   end
 
   # create data files specific to one of the classifiers for both training and testing
@@ -70,7 +79,7 @@ class PrepareData
   def write_id3_file(file, data)
     header = ""
     Attributes.each_with_index do |attribute, index|
-      header << "#{attribute.to_s}:#{Attribute_Types[index].to_s},"
+      header << "#{attribute.to_s}:#{Attribute_Types[index]},"
     end
     File.open(file, "w") do |f|
       f.puts("#{header.chomp(',')}")
@@ -113,10 +122,10 @@ class PrepareData
     end
   end
 
-  def determine_class_value(class_info)
-    if class_info[0].empty?
+  def determine_class_value(attributes)
+    if attributes[Admit_Date_Index].empty?
       "no_admit"
-    elsif class_info[1].empty?
+    elsif attributes[Enrollment_Date_Index].empty?
       "admit_no_matriculate"
     else
       "admit_matriculate"
@@ -124,6 +133,7 @@ class PrepareData
   end
 
   def convert_discrete_attribute_to_numbers(attribute, index)
+return "" if Discrete_Options[index] == 0
     attribute_option_hash = @discrete_options_to_numbers[index]
     attribute_option_hash[attribute] = attribute_option_hash.size if attribute_option_hash[attribute].nil?
 
@@ -132,3 +142,5 @@ class PrepareData
     attribute_array.to_s.gsub(/[\[\],]/, '')
   end
 end
+
+PrepareData.new("admissions_data.csv").prepare_data(:id3)
