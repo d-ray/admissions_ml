43b7b76bc937d65e308d5c1f41d976995d621033
quietfanatic <lewis@wall.org>
2014-03-03 22:49:21 -0800

    Improve naive bayes classifier code

--- naive_bayes.rb
+++ naive_bayes.rb
@@ -11,29 +11,67 @@ File.open 'admissions_data.csv' do |f|
 end
 key = split_line lines.shift
 
-class_is = 25
-discrete_is = [1, 2, 4, 5, 6, 9, 12, 15, 17, 22, 24, 26, 33]
-continuous_is = [10, 13, 30, 31, 32]  #TODO: use these
+Params = [
+  [:id, :unused],
+  [:gender, :discrete],
+  [:ethnicity, :discrete],
+  [:state, :unused],
+  [:zip, :discrete],
+  [:country, :discrete],
+  [:major1, :discrete],
+  [:major2, :unused],
+  [:major3, :unused],
+  [:hs_ceed, :discrete],
+  [:hs_gpa, :continuous],
+  [:grad_year, :unused],
+  [:act_score, :discrete],
+  [:sat_score, :continuous],
+  [:app_date, :unused],
+  [:visit_code, :discrete],
+  [:visit_date, :unused],
+  [:inquiry_code, :discrete],
+  [:admit_date, :class],  # 19
+  [:enrollment_date, :class],  # 20
+  [:deposit_date, :unused],
+  [:fsa, :unused],
+  [:et, :unused],
+  [:ot, :unused],
+  [:app_status, :unused],
+  [:scholarships, :discrete],
+  [:scholarships_bumped, :discrete],
+  [:academic_index, :unused],
+  [:academic_year, :unused],
+  [:family_income, :continuous],
+  [:family_contribution, :continuous],
+  [:total_gift_aid, :continuous],
+  [:admit_status, :discrete],
+]
 
- # counts[param index][param value][class] = count
-counts = []
+ # counts[attr name][attr value][class] = count
+counts = {}
 
 for l in lines
   params = split_line l  # TODO: commas in quotes
-  for i in discrete_is
-    if counts[i].nil? then counts[i] = {} end
-    if counts[i][params[i]].nil? then counts[i][params[i]] = {} end
-    if counts[i][params[i]][params[25]].nil? then counts[i][params[i]][params[25]] = 0 end
-    counts[i][params[i]][params[25]] += 1
+  class_val = params[19].empty? ? :no_admit : params[20].empty? ? :admit_no_matriculate : :admit_matriculate
+  Params.each_index do |i|
+    if Params[i][1] == :discrete
+      p = Params[i][0]
+      if counts[p].nil? then counts[p] = {} end
+      if counts[p][params[i]].nil? then counts[p][params[i]] = {} end
+      if counts[p][params[i]][class_val].nil? then counts[p][params[i]][class_val] = 0 end
+      counts[p][params[i]][class_val] += 1
+    end
   end
 end
 
-for i in discrete_is
-  for v in counts[i].keys
-    unless counts[i][v].nil?
-      for c in counts[i][v].keys
-        if counts[i][v][c].nil? then counts[i][v][c] = 0 end
-        puts "counts[#{key[i]}][#{v}][#{c}] = #{counts[i][v][c]}"
+for p in Params
+  next unless p[1] == :discrete
+  p = p[0]
+  for v in counts[p].keys
+    unless counts[p][v].nil?
+      for c in [:no_admit, :admit_no_matriculate, :admit_matriculate]
+        if counts[p][v][c].nil? then counts[p][v][c] = 0 end
+        puts "counts[#{p}][#{v}][#{c}] = #{counts[p][v][c]}"
       end
     end
   end
