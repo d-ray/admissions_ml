988427b4660529a7883d606015226128a5d05ff2
quietfanatic <lewis@wall.org>
2014-03-04 11:38:30 -0800

    [NB] Discretize some continuous attributes (with built-in functions)

--- naive_bayes.rb
+++ naive_bayes.rb
@@ -13,16 +13,16 @@ class NaiveBayes
     [:major2, :unused],
     [:major3, :unused],
     [:hs_ceed, :discrete],
-    [:hs_gpa, :continuous],
+    [:hs_gpa, :continuous, lambda {|gpa|discretize_gpa gpa}],
     [:grad_year, :unused],
     [:act_score, :discrete],
-    [:sat_score, :continuous],
+    [:sat_score, :continuous, lambda {|sat|discretize_sat sat}],
     [:app_date, :unused],
     [:visit_code, :discrete],
     [:visit_date, :unused],
     [:inquiry_code, :discrete],
-    [:admit_date, :class],  # 19
-    [:enrollment_date, :class],  # 20
+    [:admit_date, :unused],  # 19  These two form the class value
+    [:enrollment_date, :unused],  # 20
     [:deposit_date, :unused],
     [:fsa, :unused],
     [:et, :unused],
@@ -32,11 +32,125 @@ class NaiveBayes
     [:scholarships_bumped, :discrete],
     [:academic_index, :unused],
     [:academic_year, :unused],
-    [:family_income, :continuous],
-    [:family_contribution, :continuous],
-    [:total_gift_aid, :continuous],
+    [:family_income, :continuous, lambda {|inc|discretize_income inc}],
+    [:family_contribution, :continuous, lambda {|cont|discretize_contribution cont}],
+    [:total_gift_aid, :continuous, lambda {|gift|discretize_gift gift}],
     [:admit_status, :discrete],
   ]
+
+  def self.discretize_gpa (gpa)
+    if gpa.nil?
+      return gpa
+    end
+    gpa = gpa.to_f
+    if gpa >= 3.5
+      return "3.5..4.0"
+    elsif gpa >= 3.0
+      return "3.0..3.5"
+    elsif gpa >= 2.5
+      return "2.5..3.0"
+    elsif gpa >= 2.0
+      return "2.0..2.5"
+    elsif gpa >= 1.5
+      return "1.5..3.0"
+    elsif gpa >= 1.0
+      return "1.0..1.5"
+    else
+      return "<1.0"
+    end
+  end
+
+  def self.discretize_sat (sat)
+    if sat.nil?
+      return sat
+    end
+    sat = sat.to_i
+    if sat >= 1150
+      return "1150..1200"
+    elsif sat >= 1100
+      return "1100..1150"
+    elsif sat >= 1050
+      return "1050..1100"
+    elsif sat >= 1000
+      return "1000..1050"
+    elsif sat >= 950
+      return "950..1000"
+    elsif sat >= 900
+      return "900..950"
+    elsif sat >= 850
+      return "850..900"
+    elsif sat >= 800
+      return "800..850"
+    elsif sat >= 750
+      return "750..800"
+    elsif sat >= 700
+      return "700..750"
+    else
+      return "<700"
+    end
+  end
+
+  def self.discretize_income (inc)
+    if inc.nil?
+      return inc
+    end
+    inc = inc.to_i
+    if inc >= 500000
+      return ">=500000"
+    elsif inc >= 200000
+      return "200000..500000"
+    elsif inc >= 100000
+      return "100000..200000"
+    elsif inc >= 50000
+      return "50000..100000"
+    elsif inc >= 20000
+      return "20000..50000"
+    else
+      return "<20000"
+    end
+  end
+
+  def self.discretize_contribution (cont)
+    if cont.nil?
+      return cont
+    end
+    cont = cont.to_i
+    if cont >= 50000
+      return ">=50000"
+    elsif cont >= 20000
+      return "20000..50000"
+    elsif cont >= 10000
+      return "10000..20000"
+    elsif cont >= 5000
+      return "5000..10000"
+    elsif cont >= 2000
+      return "2000..5000"
+    else
+      return "<2000"
+    end
+  end
+
+  def self.discretize_gift (gift)
+    if gift.nil?
+      return gift
+    end
+    gift = gift.to_i
+    if gift >= 30000
+      return ">=30000"
+    elsif gift >= 25000
+      return "25000..30000"
+    elsif gift >= 20000
+      return "20000..25000"
+    elsif gift >= 15000
+      return "15000..20000"
+    elsif gift >= 10000
+      return "10000..15000"
+    elsif gift >= 5000
+      return "5000..10000"
+    else
+      return "<5000"
+    end
+  end
 
   def initialize (params = {})
     @pvs = {}
@@ -93,7 +207,7 @@ class NaiveBayes
         else
           csv << [cv.to_s, '_TOTAL', '_TOTAL', @cvs[cv][:_TOTAL]]
           Params.each_index do |i|
-            next unless Params[i][1] == :discrete
+            next if Params[i][1] == :unused
             pn = Params[i][0]
             for pv in @cvs[cv][pn].keys
               if pv == :_TOTAL
@@ -106,7 +220,7 @@ class NaiveBayes
         end
       end
       Params.each_index do |i|
-        next unless Params[i][1] == :discrete
+        next if Params[i][1] == :unused
         pn = Params[i][0]
         for pv in @pvs[pn].keys
           if pv == :_TOTAL
@@ -124,9 +238,12 @@ class NaiveBayes
     @cvs[:_TOTAL] += 1.0
     @cvs[cv][:_TOTAL] += 1.0
     Params.each_index do |i|
-      next unless Params[i][1] == :discrete
+      next if Params[i][1] == :unused
       pn = Params[i][0]
       pv = instance[i]
+      if Params[i][1] == :continuous
+        pv = Params[i][2].call(pv)
+      end
       @cvs[cv][pn] = {_TOTAL: 0.0} if @cvs[cv][pn].nil?
       @cvs[cv][pn][:_TOTAL] += 1.0
       @cvs[cv][pn][pv] = 0.0 if @cvs[cv][pn][pv].nil?
@@ -145,9 +262,12 @@ class NaiveBayes
       next if cv == :_TOTAL
       prob = @cvs[cv][:_TOTAL] / @cvs[:_TOTAL]
       Params.each_index do |i|
-        next unless Params[i][1] == :discrete
+        next if Params[i][1] == :unused
         pn = Params[i][0]
         pv = instance[i]
+        if Params[i][1] == :continuous
+          pv = Params[i][2].call(pv)
+        end
         @cvs[cv][pn][pv] = 0.0 if @cvs[cv][pn][pv].nil?
         prob *= @cvs[cv][pn][pv] / @cvs[cv][pn][:_TOTAL]
         @pvs[pn][pv] = 0.0 if @pvs[pn][pv].nil?
