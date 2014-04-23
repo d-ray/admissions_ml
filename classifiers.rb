d53b4c3f7e50160cbc9194963f989a66b0ff8058
dray <dray@westmont.edu>
2014-04-23 15:51:58 -0700

    display confusion matrices in rate_accuracy; allow user to specify a subset of the algorithms to user

--- classifiers.rb
+++ classifiers.rb
@@ -1,59 +1,107 @@
 require './prepare_data.rb'
 require './tree.rb'
 require './ann.rb'
+require './naive_bayes.rb'
+require './svm.rb'
 
 class Classifiers
 
-  Classifiers = [:id3, :ann, :nb, :svm]
+  All_Classifiers = [:id3, :ann, :nb, :svm]
   Training_Data_Folder = "training_data"
   Test_Data_Folder = "test_data"
 
-  def initialize(training_files = {}, test_files = {}, force_prepare_data = false, training_data = "admissions_data.csv")
-    Classifiers.each do |classifier|
-      training_files[classifier] ||= "#{Training_Data_Folder}/#{classifier.to_s}_training_data.txt"
-      test_files[classifier] ||= "#{Test_Data_Folder}/#{classifier.to_s}_test_data.txt"
+  def initialize(params = {})
+    @training_files = params[:training_files] || {}
+    @test_files = params[:test_files] || {}
+    @config_files = params[:config_files] || {}
+    All_Classifiers.each do |classifier|
+      @training_files[classifier] ||= "#{classifier.to_s}_training_data.txt"
+      @test_files[classifier] ||= "#{classifier.to_s}_test_data.txt"
+      @config_files[classifier] ||= "#{classifier.to_s}_config.txt"
     end
 
-    data = PrepareData.new(training_files, test_files, training_data)
-    if force_prepare_data
-      data.prepare_data(:all)
+    if params[:force_prepare_data]
+      PrepareData.new(params).prepare_data(:all)
     else
-      Classifiers.each do |classifier|
-        data.prepare_data(classifier) unless File(training_files[classifier]).exist?
+      data = PrepareData.new(params)
+      All_Classifiers.each do |classifier|
+        data.prepare_data(classifier) unless File.exist?(@training_files[classifier])
       end
     end
-    @training_files = training_files
-    @test_files = test_files
-    @classifier_instances = []
+
+    set_classifiers(params[:classifiers] || :all)
+    @classifier_instances = {}
+  end
+
+  # specify which of the classifiers should be used
+  def set_classifiers(classifier_set)
+    if classifier_set.is_a?(Symbol)
+      if classifier_set == :all
+        @classifiers = All_Classifiers
+      elsif All_Classifiers.index(classifier_set)
+        @classifiers = [classifier_set]
+      else
+        puts "Classifiers parameter '#{classifier.to_s}' is not a valid classifier"
+      end
+    elsif classifier_set.is_a?(Array)
+      @classifiers = params[:classifiers]
+    else
+      puts "Classifier set params must be of type symbol or array"
+    end
   end
 
   def train
-    @tree = Tree.new(training_file_name: @training_files[:id3])
-    @tree.train_and_save
+    initialize_classifier_training = {
+      :id3 => lambda {Tree.new(training_file_name: @training_files[:id3])},
+      :ann => lambda {Ann.new(training_file_name: @training_files[:ann])},
+      :nb => lambda {NaiveBayes.new(training_file_name: @training_files[:nb])},
+      :svm => lambda {SVM.new(training_file_name: @training_files[:svm])}
+    }
 
-    @ann = Ann.new(training_file_name: @training_files[:ann])
-    @ann.train_and_save
-
-    @nb = NaiveBayes.new(training_file_name: @training_files[:nb])
-    @nb.train_and_save
-
-    @svm = SVM.new(training_file_name: @training_files[:svm])
-    @svm.train_and_save
+    @classifiers.each {|classifier| initialize_classifier_training[classifier].call.train_and_save}
   end
 
-  def configure_from_file(config_files = {})
-    Classifiers.each {|classifier| config_files[classifier] ||= "#{classifier}_config.txt"}
-    @classifier_instances << @tree = Tree.new(config_file_name: config_files[:id3])
-    @classifier_instances << @ann = Ann.new(config_file_name: config_files[:ann])
-    @classifier_instances << @nb = NaiveBayes.new(config_file_name: config_files[:nb])
-    @classifier_instances << @svm = SVM.new(config_file_name: config_files[:svm])
+  def configure_from_file
+    initialize_classifier = {
+      :id3 => lambda {Tree.new(config_file_name: @config_files[:id3])},
+      :ann => lambda {Ann.new(config_file_name: @config_files[:ann])},
+      :nb => lambda {NaiveBayes.new(config_file_name: @config_files[:nb])},
+      :svm => lambda {SVM.new(config_file_name: @config_files[:svm])}
+    }
+
+    @classifiers.each do |classifier|
+      @classifier_instances[classifier] = initialize_classifier[classifier].call
+    end
   end
 
   def rate_accuracy
     configure_from_file if @classifier_instances.empty?
-    @classifier_instances.each {|classifier| classifier.rate_accuracy}
+    confusion_matrices = {}
+    @classifiers.each {|classifier| confusion_matrices[classifier] = @classifier_instances[classifier].rate_accuracy(@test_files[classifier])}
+    @classifiers.each do |classifier|
+      confusion_matrix = confusion_matrices[classifier]
+puts "confusion_matrix.inspect: #{confusion_matrix.inspect}"
+      total_instances = confusion_matrix.delete(:_TOTAL)
+      class_values = confusion_matrix.keys
+puts "class_values.inspect: #{class_values.inspect}"
+      
+      puts "Results for #{classifier}"
+      format = "%#{32}s\t%#{30}s\t%#{30}s\t%#{30}s\n"
+      printf(format, "", "#{class_values[0].capitalize} (Actual)", "#{class_values[1].capitalize} (Actual)", "#{class_values[2].capitalize} (Actual)")
+      class_values.each do |predicted_class|
+        printf(format, "#{predicted_class.capitalize} (Predicted)",
+        "#{confusion_matrix[class_values[0]][predicted_class]} (#{(confusion_matrix[class_values[0]][predicted_class].to_f / confusion_matrix[class_values[0]][:_TOTAL] * 100).round(2)}%)", 
+        "#{confusion_matrix[class_values[1]][predicted_class]} (#{(confusion_matrix[class_values[1]][predicted_class].to_f / confusion_matrix[class_values[1]][:_TOTAL] * 100).round(2)}%)", 
+        "#{confusion_matrix[class_values[2]][predicted_class]} (#{(confusion_matrix[class_values[2]][predicted_class].to_f / confusion_matrix[class_values[2]][:_TOTAL] * 100).round(2)}%)") 
+      end
+      correctly_classified_instances = class_values.inject(0) {|sum, class_value| sum += confusion_matrix[class_value][class_value]}
+      puts "Overall accuracy: #{correctly_classified_instances} of #{total_instances} (#{(correctly_classified_instances.to_f / total_instances * 100).round(2)}%) instances classified correctly."
+    end
   end
 end
 
-c = Classifiers.new.configure_from_file
-c.rate_accuracy
+if __FILE__ == $0
+  c = Classifiers.new(training_files: {nb: "nb_training_discretized.txt"}, test_files: {nb: "nb_test_discretized.txt"}, config_files: {nb: "nb_config.csv"}, classifiers: :nb, discretize_nb: true)
+  c.configure_from_file
+  c.rate_accuracy
+end
